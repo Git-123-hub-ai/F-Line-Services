840bf44dccc097e02c4f6b15ffb1f5dc3c521127
coding joker <71101089+Git-123-hub-ai@users.noreply.github.com>
2022-04-13 09:28:37 +0530

    underline animation with smooth
    smooth underline added with zoom effect

--- JS/expandCardsUnderline.js
+++ JS/expandCardsUnderline.js
@@ -1,39 +1,39 @@
 const home = document.getElementById("home");
 
 home.addEventListener('mouseenter',function() {
-    home.classList.add("underline","expandCard");
+    home.classList.add("expandCard");
 })
 
 home.addEventListener('mouseleave',function() {
-    home.classList.remove("underline","expandCard");
+    home.classList.remove("expandCard");
 })
 
 const aboutUs = document.getElementById("aboutUs")
 
 aboutUs.addEventListener('mouseenter' , function () {
-    aboutUs.classList.add("underline","expandCard");
+    aboutUs.classList.add("expandCard");
 })
 
 aboutUs.addEventListener('mouseleave',function(){
-    aboutUs.classList.remove("underline","expandCard");
+    aboutUs.classList.remove("expandCard");
 })
 
 const contactUs = document.getElementById("contactUs")
 
 contactUs.addEventListener('mouseenter' , function () {
-    contactUs.classList.add("underline","expandCard");
+    contactUs.classList.add("expandCard");
 })
 
 contactUs.addEventListener('mouseleave',function(){
-    contactUs.classList.remove("underline","expandCard");
+    contactUs.classList.remove("expandCard");
 })
 
 const services = document.getElementById("services")
 
 services.addEventListener('mouseenter' , function () {
-    services.classList.add("underline","expandCard");
+    services.classList.add("expandCard");
 })
 
 services.addEventListener('mouseleave',function(){
-    services.classList.remove("underline","expandCard");
+    services.classList.remove("expandCard");
 })
